50ea3846379a1358e384ef9752bf5267d96a61a1
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-07 22:01:34 +0900

    ImageSlider
    complete

--- p254/ViewController.m
+++ p254/ViewController.m
@@ -7,17 +7,60 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#define IMAGE_NUM 10
+@interface ViewController (){
+    UIScrollView *_scrollView;
+    UIPageControl *pageControl;
+    int loadedPageCount;
+}
 
 @end
 
 @implementation ViewController
-
+- (void) loadContentsPage:(int) pageNo{
+    if(pageNo <0 || pageNo <loadedPageCount||pageNo>= IMAGE_NUM)
+        return;
+    
+    float width = _scrollView.frame.size.width;
+    float height = _scrollView.frame.size.height;
+    
+    NSString *fileName = [NSString stringWithFormat:@"c%d",pageNo];
+    NSString *filePath = [[NSBundle mainBundle]pathForResource:fileName ofType:@"jpg"];
+    UIImage *image = [UIImage imageWithContentsOfFile:filePath];
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:image];
+    imageView.frame = CGRectMake(width* pageNo, 0, width, height);
+    [_scrollView addSubview:imageView];
+    loadedPageCount++;
+}
+-(void) scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    float width = scrollView.frame.size.width;
+    float offsetX =scrollView.contentOffset.x;
+    int pageNo = floor(offsetX/width);
+    pageControl.currentPage =pageNo;
+    [self loadContentsPage:pageNo-1];
+    [self loadContentsPage:pageNo];
+    [self loadContentsPage:pageNo+1];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _scrollView =[[UIScrollView alloc]initWithFrame:self.view.bounds];
+    [self.view addSubview:_scrollView];
+    
+    float height = _scrollView.bounds.size.height;
+    float width = _scrollView.bounds.size.width;
+    
+    _scrollView.delegate = self;
+    _scrollView.pagingEnabled = YES;
+    _scrollView.contentSize = CGSizeMake(width *IMAGE_NUM, height);
+    pageControl = [[UIPageControl alloc]initWithFrame:CGRectMake(130, 400, 60, 40)];
+    [self.view addSubview:pageControl];
+    pageControl.numberOfPages = IMAGE_NUM;
+    
+    loadedPageCount =0;
+    [self loadContentsPage:0];
+    [self loadContentsPage:1];
 }
 
 - (void)didReceiveMemoryWarning
